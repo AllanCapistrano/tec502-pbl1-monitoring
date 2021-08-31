6945c62451d9774a9602b36235116d20dff21e5a
AllanCapistrano <allan.capistrano3@gmail.com>
2021-08-31 14:18:19 -0300

    Add click in patient.

--- src/controllers/MonitoringController.java
+++ src/controllers/MonitoringController.java
@@ -3,17 +3,22 @@ package controllers;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import models.Patient;
+import tec502.pbl1.monitoring.Info;
 
 /**
  *
@@ -48,8 +53,36 @@ public class MonitoringController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initTable();
+        
+        table.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object clicked) {
+                selected = (Patient) clicked;
+                
+                if(selected != null){
+                    Info newWindow = new Info(selected);
+                    
+                    try {
+                        newWindow.start(new Stage());
+                    } catch (Exception ex) {
+                        System.err.println("Erro ao tentar exibir as informações do paciente.");
+                    }
+                } else{
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+
+                    alert.setTitle("Atenção");
+                    alert.setHeaderText("É necessário selecionar um paciente!");
+                    alert.show();
+                }
+                
+            }
+        
+        });
     }
 
+    /**
+     * Preenche as tabelas com as informações recebidas.
+     */
     public void initTable() {
         clmId.setCellValueFactory(new PropertyValueFactory("deviceId"));
         clmName.setCellValueFactory(new PropertyValueFactory("name"));
@@ -58,6 +91,11 @@ public class MonitoringController implements Initializable {
         table.setItems(updateTable());
     }
 
+    /**
+     * Atualiza os campos das tabelas.
+     * 
+     * @return ObservableList<Patient>
+     */
     public ObservableList<Patient> updateTable() {
         Patient p1 = new Patient("test1", "123.123.123.123");
         Patient p2 = new Patient("test2", "456.456.456.456");
@@ -72,6 +110,7 @@ public class MonitoringController implements Initializable {
         tempList.add(p4);
 
         patients = FXCollections.observableArrayList(tempList);
+        
         return patients;
     }
 
